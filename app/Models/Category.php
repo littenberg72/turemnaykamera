4b3f54b34c0dbe0c3bd1a7d53297cf213554fa1d
littenberg72 <ignatnikitenko2023@gmail.com>
2026-04-29 11:26:06 +0500

    4. Связи в моделях

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,10 +2,29 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Category extends Model
 {
+    use Sluggable;
     use HasFactory;
+
+    protected $fillable = ['title', 'slug'];
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
+
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
 }
