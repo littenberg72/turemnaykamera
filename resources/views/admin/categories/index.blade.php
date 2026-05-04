2fc499b06233985880e5405b9f8622b08872e695
littenberg72 <ignatnikitenko2023@gmail.com>
2026-05-04 09:58:08 +0500

    5. Категории №1

---
+++ resources/views/admin/categories/index.blade.php
@@ -0,0 +1,44 @@
+</div>
+@endsection
+
+@extends ('admin.layouts.layout')
+@section ('content')
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <<h1>Категории</h1>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item active">Blank Page</li>
+                    </ol>
+                </div>
+            </div>
+        </div><!--/.container-fluid -->
+    </section>
+    <!- Main content ->
+        <section class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <h3 class="card-title">Списoк категорий</h3>
+                            </div>
+                            <!--/.card-header -->
+                            <div class="card-body">
+                                <!--/.card-body -->
+                                <div class="card-footer clearfix">
+                                </div>
+                                <!--/.card -->
+                            </div>
+                            <!--/.col -->
+                        </div>
+                        <!--/.row -->
+                    </div><!--/.container-fluid -->
+        </section>
+        <!--/.content -->
